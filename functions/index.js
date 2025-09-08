20229deda8b30a5b810e6494f2811ddeda3f8f6a
AkiaPolliCamelia <mibgaryhentai@outlook.com>
2025-09-08 19:03:19 +0800

    修正http状态码笔误

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
   const redirectUrl = context.env.ROOT_REDIRECT_URL;
   if (redirectUrl) {
-    return Response.redirect(redirectUrl, 32);
+    return Response.redirect(redirectUrl, 302);
   }
   return await context.next();
 }
